e43fff1c0b3ebbb224b3e80f251dacf8d5aa1925
Nick Guimarais <Nick.guimarais@gmail.com>
2020-08-22 12:56:41 -0700

    updated app.js with inquirer prompts, updated constructor methods for each employee type, added employee roles

---
+++ app.js
@@ -0,0 +1,114 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+
+const employeesArray = [];
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your manager's name?",
+      name: "managersName"
+    },
+    {
+        type: "input",
+        message: "What is your manager's id?",
+        name: "managersid"
+    },
+    {
+        type: "input",
+        message: "What is your manager's email?",
+        name: "managersemail"
+    },
+    {
+        type: "input",
+        message: "What is your manager's office number?",
+        name: "managersnumber"
+    },
+    {
+        type: "list",
+        message: "Which type of team member would you like to add?",
+        name: "team",
+        choices: [
+            "Engineer",
+            "Intern",
+            "I don't want to add any more team members"
+        ]
+    },
+    {
+        type: "input",
+        message: `What is your ${response.choices} name?`,
+        name: `${response.choices}name`
+    },
+    {
+        type: "input",
+        message: `What is your ${response.choices} id?`,
+        name: `${response.choices}id`
+    },
+
+
+
+
+
+  ])
+
+
+//   .then(function(response) {
+
+//     if (response.confirm === response.password) {
+//       console.log("Success!");
+//     }
+//     else {
+//       console.log("You forgot your password already?!");
+//     }
+//   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Write code to use inquirer to gather information about the development team members,
+// and to create objects for each team member (using the correct classes as blueprints!)
+
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work! ```
